765f1c0f8085860e54214c24afd62ced5319cf6d
kuchikamizake05 <faaidsakhaa2005@mail.ugm.ac.id>
2026-05-04 23:07:25 +0700

    chore: update appKit

--- frontend/src/lib/web3/appKit.ts
+++ frontend/src/lib/web3/appKit.ts
@@ -1,4 +1,21 @@
 "use client";
 
-// TODO: refactor this section later
-console.log('debugging...');
+import { createAppKit } from "@reown/appkit/react";
+import {
+  solana,
+  solanaDevnet,
+  solanaTestnet,
+} from "@reown/appkit/networks";
+import type { AppKitNetwork } from "@reown/appkit/networks";
+import { SolanaAdapter } from "@reown/appkit-adapter-solana/react";
+import { SOLANA_CLUSTER } from "~/lib/web3/solana";
+
+export const SOLANA_NAMESPACE = "solana" as const;
+export const REOWN_PROJECT_ID = process.env.NEXT_PUBLIC_REOWN_PROJECT_ID || "";
+
+export function hasReownProjectId() {
+  return Boolean(REOWN_PROJECT_ID);
+}
+
+function readAppUrl() {
+  if (typeof window !== "undefined") {
